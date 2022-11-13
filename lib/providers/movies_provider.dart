378e7d772b1e79e294b2c7a22691e46c4fc7abfd
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-11-13 16:36:50 -0500

    Popular Movies

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:movies_app/models/now_playing_response.dart';
+import 'package:movies_app/models/popular_response.dart';
 import '../models/movie.dart';
 
 class MoviesProvider extends ChangeNotifier {
@@ -10,11 +11,13 @@ class MoviesProvider extends ChangeNotifier {
   String _language = 'en-US';
 
   List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
 
   MoviesProvider() {
     print('Init MoviesProvider');
 
     getOnDisplayMovies();
+    getPopularMovies();
   }
 
   getOnDisplayMovies() async {
@@ -29,6 +32,23 @@ class MoviesProvider extends ChangeNotifier {
     final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
 
     onDisplayMovies = nowPlayingResponse.results;
+
+    notifyListeners();
+  }
+
+  getPopularMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/popular', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+    });
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+    final popularResponse = PopularResponse.fromJson(response.body);
+
+    popularMovies = [...popularMovies, ...popularResponse.results];
+    print(popularMovies[0]);
 
     notifyListeners();
   }
