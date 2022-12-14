3622b12334c6bb0c14c2a0dc9aa32a2e98e1ec3f
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-12-13 23:01:23 -0500

    Search implement - show movies

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:movies_app/models/models.dart';
+import 'package:movies_app/models/search_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _apiKey = '8b15bd8ca5d9c1f90ffde85719b50276';
@@ -23,7 +24,7 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   Future<String> _getJsonData(String endpoint, [int page = 1]) async {
-    var url = Uri.https(_baseUrl, endpoint, {
+    final url = Uri.https(_baseUrl, endpoint, {
       'api_key': _apiKey,
       'language': _language,
       'page': '$page',
@@ -66,5 +67,18 @@ class MoviesProvider extends ChangeNotifier {
     moviesCast[movieId] = creditsResponse.cast;
 
     return creditsResponse.cast;
+  }
+
+  Future<List<Movie>> searchMovies(String query) async {
+    final url = Uri.https(_baseUrl, '3/search/movie', {
+      'api_key': _apiKey,
+      'language': _language,
+      'query': query,
+    });
+
+    final response = await http.get(url);
+    final searchResponse = SearchResponse.fromJson(response.body);
+
+    return searchResponse.results;
   }
 }
